3c2cb83d89a615365004323b3a899973eedcd2da
高倉楓麻 <ai24cuz@gmail.com>
2019-11-19 14:08:30 +0800

    first commit

---
+++ CameraProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CameraProject
+//
+//  Created by 高倉楓麻 on 2019/11/19.
+//  Copyright © 2019 高倉楓麻. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
